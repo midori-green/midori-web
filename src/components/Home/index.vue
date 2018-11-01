b2ee7be762247251e8b20d5563f9e019e408ea91
“jiyu” <m@jiyu.lol>
2018-11-01 23:07:42 +0900

    insert image

---
+++ src/components/Home/index.vue
@@ -0,0 +1,12 @@
+<template src="./template.html"></template>
+<style src="./style.css"></style>
+
+<script>
+export default {
+	data() {
+		return {
+			image_src: require("../../assets/美しい緑色のガラスのオブジェ(トリミング済み).png")
+		}
+	}
+}
+</script>
